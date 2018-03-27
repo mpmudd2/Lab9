3d50e3a15b23822e69235ec2fcb07c6ffe8a8e28
mpmudd2 <mpmudd2@illinois.edu>
2018-03-27 18:47:52 -0500

    did it

--- src/main/java/APITest.java
+++ src/main/java/APITest.java
@@ -16,7 +16,7 @@ public class APITest {
 
     /** Subscription key. Replace with your API key. */
     private static final String SUBSCRIPTION_KEY =
-            "13hc77781f7e4b19b5fcdd72a8df7156";
+            "80364e0eb9924638971fdbaef12a84b8";
 
     /**
      * Region to use.
@@ -29,7 +29,7 @@ public class APITest {
      * using a free trial subscription key, you should not need to change this region.
      */
     private static final String URI_BASE =
-            "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+            "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0";
 
 
     /**
